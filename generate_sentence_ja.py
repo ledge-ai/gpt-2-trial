7612874331e6bde5ee770431a10df025961c8ead
dychi <tnychor06@gmail.com>
2019-07-19 18:59:20 +0900

    日本語の生成に成功

--- generate_sentence_ja.py
+++ generate_sentence_ja.py
@@ -1,11 +1,106 @@
-from bert_juman import BertWithJumanModel
+import torch
+import torch.nn.functional as F
+
+from pyknp import Juman
+from bert_juman import BertTokenizer, BertModel
 
 
-def main():
-    bert = BertWithJumanModel('./Japanese_Model')
-    emb = bert.get_sentence_embedding("こんにちは")
-    print(emb)
+class JumanTokenizer():
+    def __init__(self):
+        self.juman = Juman()
+
+    def tokenize(self, text):
+        result = self.juman.analysis(text)
+        return [mrph.midasi for mrph in result.mrph_list()]
+
+
+def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')):
+    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+        Args:
+            logits: logits distribution shape (vocabulary size)
+            top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+            top_p > 0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+        From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+    assert logits.dim() == 1  # batch size 1 for now - could be updated for more but the code would be less clear
+    top_k = min(top_k, logits.size(-1))  # Safety check
+    if top_k > 0:
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        logits[indices_to_remove] = filter_value
+
+    if top_p > 0.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold
+        sorted_indices_to_remove = cumulative_probs > top_p
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        indices_to_remove = sorted_indices[sorted_indices_to_remove]
+        logits[indices_to_remove] = filter_value
+    return logits
+
+
+def sample_sequence(model, length, context, num_samples=1, temperature=1, top_k=0, top_p=0.0, is_xlnet=False, device='cpu'):
+    context = torch.tensor(context, dtype=torch.long)
+    context = context.unsqueeze(0).repeat(num_samples, 1)
+    generated = context
+    with torch.no_grad():
+        for _ in range(length):
+
+            inputs = {'input_ids': generated}
+            if is_xlnet:
+                # XLNet is a direct (predict same token, not next token) and bi-directional model by default
+                # => need one additional dummy token in the input (will be masked), attention mask and target mapping
+                # (see model docstring)
+                input_ids = torch.cat((generated, torch.zeros((1, 1), dtype=torch.long, device=device)), dim=1)
+                perm_mask = torch.zeros((1, input_ids.shape[1], input_ids.shape[1]), dtype=torch.float, device=device)
+                perm_mask[:, :, -1] = 1.0  # Previous tokens don't see last token
+                target_mapping = torch.zeros((1, 1, input_ids.shape[1]), dtype=torch.float, device=device)
+                target_mapping[0, 0, -1] = 1.0  # predict last token
+                inputs = {'input_ids': input_ids, 'perm_mask': perm_mask, 'target_mapping': target_mapping}
+
+            outputs = model(**inputs)  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet (cached hidden-states)
+            next_token_logits = outputs[0][0][0, -1, :] / temperature
+            filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
+            next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+            generated = torch.cat((generated, next_token.unsqueeze(0)), dim=1)
+    return generated
+
+
+def main(text):
+    bert_model = BertModel.from_pretrained('./Japanese_model/')
+    bert_tokenizer = BertTokenizer('./Japanese_Model/vocab.txt', do_lower_case=False, do_basic_tokenize=False)
+
+    juman_tokenizer = JumanTokenizer()
+
+    print("Initialize")
+    # while True:
+    tokens = juman_tokenizer.tokenize(text)
+    bert_tokens = bert_tokenizer.tokenize(" ".join(tokens))
+    ids = bert_tokenizer.convert_tokens_to_ids(["[CLS]"] + bert_tokens[:126] + ["[SEP]"])
+    # tokens_tensor = torch.tensor(ids).reshape(1, -1)
+    out = sample_sequence(
+        model=bert_model,
+        context=ids,
+        length=20,
+        temperature=1.0,
+        top_k=0,
+        top_p=0.9,
+    )
+    out = out[0, len(bert_tokens):].tolist()
+    out_list = [bert_tokenizer.ids_to_tokens[item] for item in out]
+    out_text = " ".join(out_list)
+
+    print("入力テキスト:", text)
+    print("Tokens:", bert_tokens)
+    print("出力テキスト:", out_text)
 
 
 if __name__ == "__main__":
-    main()
+    text = "適用によって、効率的な暖房効果と環境への低負荷を両立させるためには、ヒートポンプがもっとも最適な方法と言われています"
+    main(text)
